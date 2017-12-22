3125cfdf764fa7c4c52366d243f2f254e1daf476
zoli333 <nagyzoli.g@gmail.com>
2017-12-22 17:37:00 +0100

    Update Squash.java
    add licence

--- BricksGame/src/hu/webler/bricksbraker/Squash.java
+++ BricksGame/src/hu/webler/bricksbraker/Squash.java
@@ -1,4 +1,29 @@
 /*
+    Squash.java
+    Copyright (C) <2017>  <Zoltan Nagy>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    
+    This is the main program jun can compile and run with java.
+    
+    I you have any questions please send me an email to the following address:
+    nagyzoli.g@gmail.com
+    
+*/
+
+// ===========================================================================
+/*
  * This is a simple bricks breaker program written in java. Hopefully somebody will find
  * it useful.
  * 
