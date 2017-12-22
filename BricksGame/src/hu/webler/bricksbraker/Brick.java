7a7c6411c4c3867a26cb9d24825d0f6b6be6121c
zoli333 <nagyzoli.g@gmail.com>
2017-12-22 17:35:37 +0100

    Update Brick.java

--- BricksGame/src/hu/webler/bricksbraker/Brick.java
+++ BricksGame/src/hu/webler/bricksbraker/Brick.java
@@ -1,3 +1,25 @@
+/*
+    Brick.java
+    Copyright (C) <2017>  <Zoltan Nagy>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    
+    I you have any questions please send me an email to the following address:
+    nagyzoli.g@gmail.com
+    
+*/
+
 package hu.webler.bricksbraker;
 
 import java.awt.Color;
