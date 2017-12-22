36a3f13631e132cdf7ed4877675cd76d6119a60f
zoli333 <nagyzoli.g@gmail.com>
2017-12-22 17:48:29 +0100

    Update Ball.java

--- BricksGame/src/hu/webler/bricksbraker/Ball.java
+++ BricksGame/src/hu/webler/bricksbraker/Ball.java
@@ -1,6 +1,5 @@
 /*
-    Ball.java
-    Copyright (C) 2017  Author: Zoltan Nagy
+    <Ball.java>  Copyright (C) <2017>  <Zoltan Nagy>
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
